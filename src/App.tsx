e689e060df157ae959d8610fa3b926f9e5f8f958
Grant Davis <grantdavis303@gmail.com>
2025-07-10 12:08:05 -0600

    Update Main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,48 @@
 import { useState } from 'react'
-import { hexlify, parseEther, ethers } from 'ethers'
+import { hexlify, parseEther, formatEther, ethers } from 'ethers'
 import './App.css'
 
 import ETHRegistrarControllerABI from './abis/ETHRegistrarController.json'
 import BaseRegistrarABI from './abis/BaseRegistrarImplementation.json'
 import PublicResolverABI from './abis/PublicResolver.json'
 
+// User Information
+const alchemyKey = import.meta.env.VITE_ALCHEMY_API_KEY
+const walletPrivateKey = import.meta.env.VITE_WALLET_PRIVATE_KEY
+
+console.log('Alchemy API Key: ', alchemyKey)
+console.log('Wallet Private Key: ', walletPrivateKey)
+
+const wallet = new ethers.Wallet(walletPrivateKey);
+
+console.log('Wallet: ', wallet)
+console.log('Wallet Address: ', wallet.address)
+
+const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
+const signer = wallet.connect(provider)
+
+console.log('Provider: ', provider)
+console.log('Signer: ', signer)
+
 function App() {
+  // Converting ETH and Wei
   const [ethNum, setEthNum] = useState('')
   const [parsedEth, setParsedEth] = useState(0n)
+  const [weiNum, setWeiNum] = useState('')
+  const [parsedWei, setParsedWei] = useState('0')
+
+  // Blockchain Information
   const [blockNumber, setBlockNumber] = useState(0)
+
+  // Wallet Information
   const [address, setAddress] = useState('')
   const [accountBalance, setAccountBalance] = useState(0n)
 
+  // ENS Information
   const [ensName, setEnsName] = useState('')
   const [ensNameAddress, setEnsNameAddress] = useState('')
   const [ensDomainName, setEnsDomainName] = useState('')
-
-  const [ensDomainNameSmartContract, setEnsDomainNameSmartContract] = useState('')
-
-  // const alchemyKey = import.meta.env.VITE_ALCHEMY_API_KEY
-  // const walletPrivateKey = import.meta.env.VITE_WALLET_PRIVATE_KEY
-
-  // console.log('Alchemy API Key: ', alchemyKey)
-  // console.log('Wallet Private Key: ', walletPrivateKey)
-
-  // const wallet = new ethers.Wallet(walletPrivateKey);
-
-  // console.log('Wallet: ', wallet)
-  // console.log('Wallet Address: ', wallet.address)
-
-  // const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
-  // const signer = wallet.connect(provider)
-
-  // console.log('Provider: ', provider)
-  // console.log('Signer: ', signer)
-
-  // // Contracts on Sepolia
-  // const ETHRegistrarControllerAddress = '0xfb3cE5D01e0f33f41DbB39035dB9745962F1f968'
-  // const BaseRegistrarAddress = '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85'
-  // const PublicResolverAddress = '0xE99638b40E4Fff0129D56f03b55b6bbC4BBE49b5'
-
-  // console.log('ETHRegistrarControllerAddress: ', ETHRegistrarControllerAddress)
-  // console.log('BaseRegistrarAddress: ', BaseRegistrarAddress)
-  // console.log('PublicResolverAddress: ', PublicResolverAddress)
-
-  // // ABIs (Imported Up Top)
-  // console.log('ETHRegistrarControllerABI: ', ETHRegistrarControllerABI)
-  // console.log('BaseRegistrarABI: ', BaseRegistrarABI)
-  // console.log('PublicResolverABI: ', PublicResolverABI)
-
-  // // Registration Data
-  // const name = 'grantdavis303'
-  // const duration = 60 * 60 * 24 * 365 // 31_536_000
-  // const secret = hexlify(ethers.randomBytes(32))
-
-  // console.log('Name: ', name)
-  // console.log('Duration: ', duration)
-  // console.log('Secret: ', secret)
-
-  // async function createName() {
-  //   const registrationObject = {
-  //     label: name,
-  //     owner: ETHRegistrarControllerAddress,
-  //     duration: duration,
-  //     secret: secret,
-  //     resolver: '0x0000000000000000000000000000000000000000',
-  //     data: [],
-  //     reverseRecord: 0,
-  //     referrer: '0x0000000000000000000000000000000000000000000000000000000000000000'
-  //   }
-
-  //   const controller = new ethers.Contract(
-  //     ETHRegistrarControllerAddress,
-  //     ETHRegistrarControllerABI.abi,
-  //     signer
-  //   )
-
-  //   console.log('Controller: ', controller)
-
-  //   const commitment = await controller.makeCommitment(registrationObject)
-
-  //   console.log('Commitment: ', commitment)
-
-  //   console.log('Sending commit...')
-
-  //   const tx1 = await controller.commit(commitment)
-  //   await tx1.wait()
-
-  //   console.log('Commit sent. Waiting 60 seconds...')
-
-  //   await new Promise ((r) => setTimeout(r, 60000))
-
-  //   console.log('Waited 60 seconds!')
-  //   console.log('Registering...!')
-
-  //   const tx2 = await controller.register(registrationObject, {
-  //     value: BigInt('3125000000003490')
-  //   })
-
-  //   // const tx2 = await controller.register(name, wallet.address, duration, secret, {
-  //   //   value: await controller.rentPrice(name, duration)
-  //   // })
-
-  //   await tx2.wait()
-
-  //   console.log(`ENS name "${name}.eth" registered!`)
-  // }
+  const [ensDomainNameMessage, setEnsDomainNameMessage] = useState('')
 
   function parseEth(num: string) {
     const number = num.toString()
@@ -117,8 +53,16 @@ function App() {
     return parsedEthNum
   }
 
+  function formatWei(num: string) {
+    const number = num.toString()
+    const parsedWeiNum = formatEther(number)
+
+    setParsedWei(parsedWeiNum)
+
+    return parsedWeiNum
+  }
+
   async function checkBlockNumber() {
-    const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
     const blockNumber = await provider.getBlockNumber();
 
     setBlockNumber(blockNumber)
@@ -127,11 +71,7 @@ function App() {
   }
 
   async function getAccountBalance(account: string) {
-    const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
     const balance = await provider.getBalance(account)
-
-    console.log('Account: ', account)
-    console.log('Account Type: ', typeof account)
 
     setAccountBalance(balance)
 
@@ -139,11 +79,7 @@ function App() {
   }
 
   async function resolveName(ensName: string) {
-    const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
     const address = await provider.resolveName(ensName)
-
-    console.log('ENS Name: ', ensName)
-    console.log('Address: ', address)
 
     setEnsNameAddress(address)
 
@@ -151,22 +87,7 @@ function App() {
   }
 
   function createEnsSepoliaDeployment() {
-    const alchemyKey = import.meta.env.VITE_ALCHEMY_API_KEY
-    const walletPrivateKey = import.meta.env.VITE_WALLET_PRIVATE_KEY
-
-    console.log('Alchemy API Key: ', alchemyKey)
-    console.log('Wallet Private Key: ', walletPrivateKey)
-
-    const wallet = new ethers.Wallet(walletPrivateKey);
-
-    console.log('Wallet: ', wallet)
-    console.log('Wallet Address: ', wallet.address)
-
-    const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
-    const signer = wallet.connect(provider)
-
-    console.log('Provider: ', provider)
-    console.log('Signer: ', signer)
+    setEnsDomainNameMessage(`Registering ENS domain name...`)
 
     // Contracts on Sepolia
     const ETHRegistrarControllerAddress = '0xfb3cE5D01e0f33f41DbB39035dB9745962F1f968'
@@ -183,13 +104,15 @@ function App() {
     console.log('PublicResolverABI: ', PublicResolverABI)
 
     // Registration Data
-    const name = ensDomainName // 'grantdavis303'
+    const name = ensDomainName
     const duration = 60 * 60 * 24 * 365 // 31_536_000
     const secret = hexlify(ethers.randomBytes(32))
 
     console.log('Name: ', name)
     console.log('Duration: ', duration)
     console.log('Secret: ', secret)
+
+    setEnsDomainNameMessage(`Creating domain name for ${name}.eth...`)
 
     createName()
 
@@ -218,141 +141,153 @@ function App() {
       console.log('Commitment: ', commitment)
 
       console.log('Sending commit...')
+      setEnsDomainNameMessage('Sending commit...')
 
       const tx1 = await controller.commit(commitment)
       await tx1.wait()
 
       console.log('Commit sent. Waiting 60 seconds...')
+      setEnsDomainNameMessage('Commit sent. Waiting 60 seconds...')
 
       await new Promise ((r) => setTimeout(r, 60000))
 
       console.log('Waited 60 seconds!')
-      console.log('Registering...!')
+      setEnsDomainNameMessage('Waited 60 seconds!')
+
+      console.log('Registering...')
+      setEnsDomainNameMessage('Registering...')
 
       const tx2 = await controller.register(registrationObject, {
         value: BigInt('3125000000003490') // 0.003125 ETH
       })
 
-      // const tx2 = await controller.register(name, wallet.address, duration, secret, {
-      //   value: await controller.rentPrice(name, duration)
-      // })
-
       await tx2.wait()
 
       console.log(`ENS name "${name}.eth" registered!`)
+      setEnsDomainNameMessage(`ENS name "${name}.eth" registered!`)
+
+      console.log(`See ENS profile here: https://sepolia.app.ens.domains/${name}.eth`)
+      setEnsDomainNameMessage(`See ENS profile here: https://sepolia.app.ens.domains/${name}.eth`)
     }
   }
 
-  function createEnsSepoliaDeploymentSmartContract() {
-    const alchemyKey = import.meta.env.VITE_ALCHEMY_API_KEY
-    const walletPrivateKey = import.meta.env.VITE_WALLET_PRIVATE_KEY
-
-    console.log('Alchemy API Key: ', alchemyKey)
-    console.log('Wallet Private Key: ', walletPrivateKey)
-
-    const wallet = new ethers.Wallet(walletPrivateKey);
-
-    console.log('Wallet: ', wallet)
-    console.log('Wallet Address: ', wallet.address)
-
-    const provider = new ethers.JsonRpcProvider(`https://eth-sepolia.g.alchemy.com/v2/${alchemyKey}`)
-    const signer = wallet.connect(provider)
-
-    // Registration Data
-    const name = ensDomainNameSmartContract
-    const contractAddress = '0x'
-
-    console.log('Name: ', name)
-  }
-
   return (
-    <>
-      <h1> Ethereum Playground </h1>
+    <div className='contentContainer'>
+      <h1 className='mainHeader'> Ethereum Playground </h1>
 
+      <h2> User Information </h2>
+
+      {/* User Information */}
+      <div className='block user'>
+        <p><span className='bold'>Wallet Public Address:</span> {wallet.address} </p>
+      </div>
+
+      <h2> Unit Conversion </h2>
+
+      {/* Convert ETH to Wei */}
       <div className='block'>
-        <h2> Parsing Ether </h2>
+        <h2> Convert ETH to Wei </h2>
 
+        <p><span className='bold'>ETH:</span></p>
         <input type="number" onChange={(e) => setEthNum(e.target.value)} />
 
         <div>
-          <button onClick={() => {parseEth(ethNum)}}> Parse {ethNum} Eth </button>
+          <button onClick={() => {parseEth(ethNum)}}> Convert {ethNum} ETH to Wei </button>
         </div>
 
-        <p> Minimum: 0.000000000000000001 </p>
-        <p> Parsed Eth: {parsedEth} wei </p>
+        <p><span className='bold'>Converted ETH:</span> {parsedEth} wei </p>
       </div>
 
+      {/* Convert Wei to ETH */}
       <div className='block'>
-        <h2> Getting Current Block Number </h2>
+        <h2> Convert Wei to ETH </h2>
+
+        <p><span className='bold'>Wei:</span></p>
+        <input type="number" onChange={(e) => setWeiNum(e.target.value)} />
+
+        <div>
+          <button onClick={() => {formatWei(weiNum)}}> Convert {weiNum} Wei to ETH </button>
+        </div>
+
+        <p><span className='bold'>Converted Wei:</span> {parsedWei} ETH </p>
+      </div>
+
+      <h2> Blockchain </h2>
+
+      {/* Get Current Block Number */}
+      <div className='block'>
+        <h2> Get Current Block Number </h2>
 
         <div>
           <button onClick={() => {checkBlockNumber()}}> Check Current Block # </button>
         </div>
 
-        <p> Block # (Sepolia): {blockNumber} </p>
+        <p><span className='bold'>Current Block Number on Sepolia:</span> {blockNumber} </p>
       </div>
 
+      <h2> Wallets </h2>
+
+      {/* Get Current Account Balance */}
       <div className='block'>
-        <h2> Getting Account Balance </h2>
+        <h2> Get Current Account Balance </h2>
 
-        <p> You can use a public Hex address (0x...) or an ENS domain. </p>
+        <ul>
+          <li> You can use a public Hex address (0x...) or an ENS domain. </li>
+          <li> Try it with <i>vitalik.eth</i> or <i>0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045</i>. </li>
+        </ul>
 
+        <p><span className='bold'>Account Address:</span></p>
         <input type="text" onChange={(e) => setAddress(e.target.value)} />
 
         <div>
           <button onClick={() => {getAccountBalance(address)}}> Get Account Balance </button>
         </div>
 
-        <p> Account Address (Sepolia): {address} </p>
-        <p> Account Balance (Sepolia): {accountBalance} wei </p>
+        <p><span className='bold'>Account Address on Sepolia:</span> {address} </p>
+        <p><span className='bold'>Account Balance on Sepolia:</span> {accountBalance} wei </p>
       </div>
 
+      <h2> ENS </h2>
+
+      {/* Resolve an ENS Name */}
       <div className='block'>
-        <h2> Resolve ENS Name </h2>
+        <h2> Resolve an ENS Name </h2>
 
-        <p> Try it with <i>vitalik.eth</i> </p>
+        <p> Resolving an ENS name means to load information about an ENS name, like it's address. </p>
 
+        <ul>
+          <li> Try it with <i>vitalik.eth</i>. </li>
+        </ul>
+
+        <p><span className='bold'>ENS Name to Resolve:</span></p>
         <input type="text" onChange={(e) => setEnsName(e.target.value)} />
 
         <div>
           <button onClick={() => {resolveName(ensName)}}> Resolve ENS Name </button>
         </div>
 
-        <p> ENS Name Address (Sepolia): {ensNameAddress} </p>
+        <p><span className='bold'>ENS Name Address on Sepolia:</span> {ensNameAddress} </p>
       </div>
 
+      {/* Register an ENS Domain Name */}
       <div className='block'>
-        <h2> ENS Sepolia Deployment </h2>
+        <h2> Register an ENS Domain Name </h2>
 
-        <p> Desired ENS Name: </p>
+        <ul>
+          <li> Make sure to change your private key in the .env file if the account already has a registered ENS domain. </li>
+          <li> Make sure to have enough ETH in your account (&#62; 0.0032 ETH) for gas fees. </li>
+        </ul>
+
+        <p><span className='bold'>Desired ENS Name:</span></p>
         <input type="text" onChange={(e) => setEnsDomainName(e.target.value)} />
-
-        <p> Be sure to change your private key in the .env file </p>
-        <p> Be sure to have enough ETH in your account </p>
 
         <div>
           <button onClick={() => {createEnsSepoliaDeployment()}}> Create ENS Domain Name </button>
         </div>
 
-        <p> Message: {} </p>
+        <p><span className='bold'>Message:</span> {ensDomainNameMessage} </p>
       </div>
-
-      <div className='block'>
-        <h2> ENS Sepolia Deployment for Smart Contracts </h2>
-
-        <p> Desired ENS Name: </p>
-        <input type="text" onChange={(e) => setEnsDomainNameSmartContract(e.target.value)} />
-
-        <p> Be sure to change your private key in the .env file </p>
-        <p> Be sure to have enough ETH in your account </p>
-
-        <div>
-          <button onClick={() => {createEnsSepoliaDeploymentSmartContract()}}> Create ENS Domain Name (Smart Contract) </button>
-        </div>
-
-        <p> Message: {} </p>
-      </div>
-    </>
+    </div>
   )
 }
 
